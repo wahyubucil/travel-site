9d440ad6e839ce6fa1d9ec65b49356266af61ecd
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-06-18 03:01:35 +0800

    Create the task for styles and some testing on css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
 
 gulp.task('default', function() {
     console.log('Hooray - This is the gulp task');
@@ -10,7 +14,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-    console.log('Imagine do something for css task');
+    return gulp.src('./app/assets/styles/styles.css')
+            .pipe(postcss([cssvars, nested, autoprefixer]))
+            .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
