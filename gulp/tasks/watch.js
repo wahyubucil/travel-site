2ba2cb7bd6606bdc8848d84e03345d74ee416db7
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-09 16:55:55 +0800

    Completed the build task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create(); 
+    browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
 
