1d6c3d5974af2eac97ee211cd18b1dc998159b93
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-09 17:04:14 +0800

    New config on build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -16,7 +16,7 @@ gulp.task('previewDist', function() {
     });
 });
 
-gulp.task('deleteDistFolder', function() {
+gulp.task('deleteDistFolder', ['icons'], function() {
     return del('./docs');
 });
 
@@ -35,7 +35,7 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
         .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
+gulp.task('optimizeImages', ['deleteDistFolder'], function() {
     return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
         .pipe(imagemin({
             progressive: true,
@@ -45,7 +45,11 @@ gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
         .pipe(gulp.dest('./docs/assets/images'));
 });
 
-gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
+gulp.task('useminTrigger', ['deleteDistFolder'], function() {
+    gulp.start('usemin');
+});
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
     return gulp.src('./app/index.html')
         .pipe(usemin({
             css: [function() {return rev()}, function() {return cssnano()}],
@@ -54,4 +58,4 @@ gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
         .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'useminTrigger']);
