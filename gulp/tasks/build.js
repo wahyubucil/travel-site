3a02bc6460264a5337e88098d880d8873dd591af
Wahyu Budi Saputra <wahyubucil@gmail.com>
2018-07-09 16:57:21 +0800

    Change dist to docs

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,13 +11,13 @@ gulp.task('previewDist', function() {
     browserSync.init({
         notify: false,
         server: {
-            baseDir: 'dist'
+            baseDir: 'docs'
         }
     });
 });
 
 gulp.task('deleteDistFolder', function() {
-    return del('./dist');
+    return del('./docs');
 });
 
 gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
@@ -32,7 +32,7 @@ gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
     ];
 
     return gulp.src(pathsToCopy)
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
@@ -42,7 +42,7 @@ gulp.task('optimizeImages', ['deleteDistFolder', 'icons'], function() {
             interlace: true,
             multipass: true
         }))
-        .pipe(gulp.dest('./dist/assets/images'));
+        .pipe(gulp.dest('./docs/assets/images'));
 });
 
 gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
@@ -51,7 +51,7 @@ gulp.task('usemin', ['deleteDistFolder', 'styles', 'scripts'], function() {
             css: [function() {return rev()}, function() {return cssnano()}],
             js: [function() {return rev()}, function() {return uglify()}]
         }))
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'optimizeImages', 'usemin']);
